6888f96bed1e7acf6e44d4ac20c9dad735822100
faizan <faizan@faizans-imac.local>
2016-06-16 20:12:50 +0530

    Initial Commit

---
+++ jokes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  jokes
+//
+//  Created by faizan on 16/06/16.
+//  Copyright © 2016 faizan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
